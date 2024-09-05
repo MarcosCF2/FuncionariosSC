75acd3243854e46bc6619caa66a123613a3f14da
MarcosCF2 <marcosfernandocarvalho2@gmail.com>
2024-09-04 21:46:55 -0300

    Trabalho

---
+++ src/main/java/br/edu/famper/funcionariossc/FuncionariosScApplication.java
@@ -0,0 +1,13 @@
+package br.edu.famper.funcionariossc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionariosScApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuncionariosScApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/famper/funcionariossc/FuncionariosScApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.famper.funcionariossc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionariosScApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
